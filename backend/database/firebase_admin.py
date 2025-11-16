e5c78a842dbbc3bdf5bfe4e58bc179f665d958f5
Angel <angel.xp.pb@gmail.com>
2025-11-16 17:09:34 -0500

    fix: firebase admin using env json

--- backend/database/firebase_admin.py
+++ backend/database/firebase_admin.py
@@ -1,20 +1,32 @@
 # backend/database/firebase_admin.py
+
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, db
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-SERVICE_KEY_PATH = BASE_DIR / "serviceAccountKey.json"
 
 def init_firebase():
     """
-    Inicializa Firebase Admin SDK.
+    Inicializa Firebase Admin usando la variable de entorno
+    GOOGLE_APPLICATION_CREDENTIALS_JSON
     """
-    if not firebase_admin._apps:
-        cred = credentials.Certificate(str(SERVICE_KEY_PATH))
+    if firebase_admin._apps:
+        return firebase_admin.get_app()
 
-        firebase_admin.initialize_app(cred, {
-            "databaseURL": "https://secrets-74e91-default-rtdb.firebaseio.com/"
-        })
+    # Leer JSON desde variable de entorno
+    json_str = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+
+    if not json_str:
+        raise RuntimeError("ERROR: GOOGLE_APPLICATION_CREDENTIALS_JSON NO ESTÁ DEFINIDO")
+
+    # Convertir string → diccionario
+    service_account_info = json.loads(json_str)
+
+    # Crear credenciales desde dict
+    cred = credentials.Certificate(service_account_info)
+
+    firebase_admin.initialize_app(cred, {
+        "databaseURL": "https://secrets-74e91-default-rtdb.firebaseio.com/"
+    })
 
     return db
